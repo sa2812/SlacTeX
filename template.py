6a9271fa3a3e907bf9962cafa0c2a3981515cee9
Sunny Amrat <sa2812@imperial.ac.uk>
2016-12-22 21:28:28 +0000

    Restored template.py

---
+++ template.py
@@ -0,0 +1,20 @@
+import os
+import jinja2
+from jinja2 import Template
+
+latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+template = latex_jinja_env.get_template('templates/jinja-test.tex')
+with open('output/output.tex', 'wb') as output:
+	output.write(template.render(section1='Long Form', section2='Short Form'))
